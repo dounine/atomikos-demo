4f0e424d5bb511d16cb99c0252e56c93f99f8423
huanghuanlai <102535481@qq.com>
2016-11-15 15:22:20 +0800

    增加方法调用响应结果说明

--- jdbc-example/src/test/java/jdbc/JDBCTest.java
+++ jdbc-example/src/test/java/jdbc/JDBCTest.java
@@ -18,6 +18,10 @@ public class JDBCTest {
     @Autowired
     private IUserService userService;
 
+    /**
+     * 此方法会执行会抛出DaoException异常,但是两个库的表并不会插入数据
+     * 要想成功插入可把UserDao中的异常代码处删除,两个数据库可正常插入数据
+     */
     @Test
     public void testSave(){
         User user = new User();
